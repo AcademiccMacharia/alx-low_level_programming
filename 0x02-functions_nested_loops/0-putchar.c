80088502f366e52180817fbe48812bae7bba27a6
AcademiccMacharia <machariab19@gmail.com>
2022-07-05 04:59:46 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,16 @@
  * Return - On Success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int main(void)
 {
-	char c = _putchar;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114}
+	int count, sz;
 
-	return (write(1, &c, 1));
+	sz = sizeof(str) / sizeof(int);
+	for (count=0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return(0);
 }
