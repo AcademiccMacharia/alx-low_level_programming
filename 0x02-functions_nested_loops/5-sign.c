51263b519c35462cf9298ddd9f81deec854e14c1
AcademiccMacharia <machariab19@gmail.com>
2022-07-05 09:03:08 -0700

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,18 +11,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar(43);
 		return (1);
-		_putchar('+');
 	}
 	else if (n == 0)
 	{
 		return (0);
-		_putchar('0');
+		_putchar(45);
 	}
 	else
 	{
 		return (-1);
-		_putchar('-');
+		_putchar(48);
 	}
 	_putchar('\n');
 }
