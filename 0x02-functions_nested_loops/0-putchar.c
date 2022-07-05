9eb0058e7edc2583792150fbe9936d94adbb4067
AcademiccMacharia <machariab19@gmail.com>
2022-07-05 01:22:31 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Function that prints _putchar
+ *i
+ *
+ * Return - Always (0) Success
+ */
+int main()
+{
+	int c;
+
+	c = _putchar;
+	putchar(c);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+###Contents of this directory.
+ - Nested while loops
+ - C- Functions
+ - Learning to Program in C
+ - What is the purpose of a function prototype?
+ - C - Header files
