4a9c26c9416fc44470d37ae5dace7cedc9cdc681
AcademiccMacharia <machariab19@gmail.com>
2022-07-14 07:23:16 -0700

    fixing errors!

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
-#include <string.h>
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
