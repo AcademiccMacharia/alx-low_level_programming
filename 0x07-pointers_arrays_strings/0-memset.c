088814e293484e95d05bf712d471452459e10f6e
AcademiccMacharia <machariab19@gmail.com>
2022-07-18 05:50:23 -0700

    fixing betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: content to be written
  *
- *
- *
+ * Return: returns a pointer to the memory area
  */
 
 char *_memset(char *s, char b, unsigned int n)
--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,7 +1,6 @@
 #include <unistd.h>
 
 /**
- *
  * _putchar - writes the character c to stdout
  *  @c: The character to print
  * Return: On success 1
