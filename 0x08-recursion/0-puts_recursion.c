9bfa65fcbb08f327e2b580a0ae8c771cbca04425
AcademiccMacharia <machariab19@gmail.com>
2022-07-20 08:36:04 -0700

    Function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -prints a string
+ * @s: string to be printed
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		_putchar(s + 1);
+	}
+	_putchar('\n');
+}
