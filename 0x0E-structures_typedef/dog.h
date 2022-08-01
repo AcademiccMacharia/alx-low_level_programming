041da2f8a5e6119e82fc7f6a0847929ff64500f9
AcademiccMacharia <machariab19@gmail.com>
2022-08-01 16:09:40 +0300

    fixing task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
-#ifndef _DOG_H
-#define _DOG_H
-
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct that stores information about a dog.
+ * @name - name of the dog
+ * @owner - owner of the dog
+ * @age - age of the dog
+ *
+ */
 struct dog
 {
 	char *name;
