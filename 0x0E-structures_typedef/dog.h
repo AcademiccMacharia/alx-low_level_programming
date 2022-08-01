1dfdd5e2892a2230f4e3d93472ada5888e9b9b72
AcademiccMacharia <machariab19@gmail.com>
2022-08-01 16:01:30 +0300

    changes to header and task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
-int _putchar(char c);
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+int _putchar(char c);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
