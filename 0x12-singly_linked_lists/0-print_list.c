cbf033c8e3da0ea50cfd2ac03d070e1e8eddca71
AcademiccMacharia <machariab19@gmail.com>
2022-08-11 16:28:45 +0300

    fixing betty errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,23 +3,23 @@
 /**
  * print_list - prints all the elements of a list
  * @h: singly linked list
- * Return: The numbere of elements in the list
- * 
+ * Return: The number of elements in the list
+ *
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t lem;
+	size_t nelem;
 
-	lem = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		       printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d]" %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		lem++;
+		nelem++;
 	}
-	return (lem);
-}	
+	return (nelem);
+}
